ac9c9386bef82e1347504c328e2354339ee4612d
arda0605 <arda.gul@ug.bilkent.edu.tr>
2024-09-25 15:50:15 +0300

    sum of even odd added

--- Homework.java
+++ Homework.java
@@ -22,4 +22,27 @@ public class Homework {
         }
         return randomArray;
     }
+
+    /**
+     * Find the sum of elements with odd- and even-numbered indexes
+     * @param array
+     * @return new array with the index 0 shows sum of evens. index 1 shows sum of odds
+     */
+    public static int[] evenOdd(int[] array){
+        int sumEven=0;
+        int sumOdd=0;
+        for(int i=0;i<array.length;i++){
+            if(array[i]%2==0){
+                sumEven+=array[i];
+            }
+            else{
+                sumOdd+=array[i];
+            }
+        }
+        int[] arr=new int[2];
+        arr[0]=sumEven;
+        arr[1]=sumOdd;
+        return arr;
+        
+    }
 }
